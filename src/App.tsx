1ea535452f191eaa6c5ad402940f98ef43e2179b
Solly <solobarine@gmail.com>
2023-10-05 17:11:26 +0100

    Save and Load Data from Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Category from './Category'
 import { Category as CategoryInterface } from './interface/hierachy'
 import Service from './Service'
@@ -6,16 +6,23 @@ import seedData from './seedData'
 import './App.css'
 
 const App = () => {
-  const [data, setData] = useState(seedData)
+  const [data, setData] = useState<never[] | CategoryInterface[]>([])
+  const savedData = localStorage.getItem('categories') as string
   console.log(data);
 
   const updateState = (state: CategoryInterface[]) => {
     setData(state)
   }
+
+  useEffect(() => {
+    (savedData)
+    ? setData(JSON.parse(savedData))
+    : setData(seedData)
+  }, [])
   
   return (
     <div>
-      <h1>Abyss Test</h1>
+      <h3>Abyss Test</h3>
       {
         data.map((item, index) => (
           (item.children)
